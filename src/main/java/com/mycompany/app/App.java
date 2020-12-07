758afddf9056b6db1917ee36439ac2ec0794e9cc
zhangqian <415677303@qq.com>
2020-12-07 14:25:53 +0800

    change message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! 张迁 demo 自动构建";
+    private final String message = "Hello World! 张迁 demo 自动构建w";
 
     public App() {}
 
